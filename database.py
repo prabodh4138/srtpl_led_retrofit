eeec3963e1d056d079b30d9c52a7d01112dc35f4
prabodh4138 <prabodh4138@gmail.com>
2026-03-01 11:41:43 +0530

    Refactor database connection handling
    Refactor database connection logic to raise an error if DATABASE_URL is not set and add SSL mode to PostgreSQL connection.

--- database.py
+++ database.py
@@ -1,28 +1,27 @@
 import os
 from sqlalchemy import create_engine
-from sqlalchemy.orm import sessionmaker
+from sqlalchemy.orm import sessionmaker, declarative_base
+from dotenv import load_dotenv
 
-# -----------------------------------
-# DATABASE URL LOGIC
-# -----------------------------------
+load_dotenv()
 
 DATABASE_URL = os.getenv("DATABASE_URL")
 
-if DATABASE_URL:
-    # Render PostgreSQL
-    engine = create_engine(
-        DATABASE_URL,
-        pool_pre_ping=True
-    )
-else:
-    # Local SQLite
-    engine = create_engine(
-        "sqlite:///srtpl.db",
-        connect_args={"check_same_thread": False}
-    )
+if not DATABASE_URL:
+    raise ValueError("DATABASE_URL not set")
+
+engine = create_engine(
+    DATABASE_URL,
+    pool_pre_ping=True,
+    connect_args={
+        "sslmode": "require"
+    }
+)
 
 SessionLocal = sessionmaker(
     autocommit=False,
     autoflush=False,
     bind=engine
 )
+
+Base = declarative_base()
